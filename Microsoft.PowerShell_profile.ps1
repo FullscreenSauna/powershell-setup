5aea40e2470fef25afdbcb7eade58ea57b6cb495
Aleksandar Dzhekov <aleksandar.dzhekov@gmail.com>
2022-10-13 22:42:36 +0300

    use relative position for user in powershell config file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 <# oh-my-posh init #>
-oh-my-posh init pwsh --config 'C:\Users\aleksandar.dzhekov\AppData\Local\Programs\oh-my-posh\themes\peru-dracula.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config '~\AppData\Local\Programs\oh-my-posh\themes\peru-dracula.omp.json' | Invoke-Expression
